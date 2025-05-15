400dc2495a9cdd20072ec4f93a189c08ff16a12b
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-05-16 01:35:59 +0530

    feat : service  - auth and prisma

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+   
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Request, Res, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from './guards/google-auth/google-auth.guard';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/login')
+  googleLogin() {}
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/callback')
+  async googleCallback(@Request() req, @Res() res: Response) {
+    // console.log('user:', req.user);
+
+    const userData = await this.authService.login(req.user);
+
+    res.redirect(
+      `http://localhost:3000/api/auth/google/callback?userId=${userData.id}&name=${userData.name}&avatar=${userData.avatar}&accessToken=${userData.accessToken}`,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('verify-token')
+  verify() {
+    return 'ok';
+  }
+}
